1df7410b0e99ac371b8f6f7d2151bd1ef1376c74
yamamura-k <yamayama23bb@gmail.com>
2021-01-18 18:58:31 +0900

    Modify script structure

--- ver02/utils/utils.py
+++ ver02/utils/utils.py
@@ -1,11 +1,14 @@
 import os
 import pickle
+#STORAGE = './result/pkl/'
+STORAGE = './result1031/pkl/'
+#STORAGE = './new/'
 
 def preserve(v, filename):
-    with open(os.path.join('./result/pkl/',filename+".pkl"), 'wb') as f:
+    with open(os.path.join(f'{STORAGE}',filename+".pkl"), 'wb') as f:
         pickle.dump(v,f)
 
 def Load(filename):
-    with open(os.path.join('./result/pkl/',filename),'rb') as f:
+    with open(os.path.join(f'{STORAGE}',filename),'rb') as f:
         v = pickle.load(f)
     return v
